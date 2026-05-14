554ee880bbe193424747c330936a9c0dacfa514e
김이레 <165885725+elroi773@users.noreply.github.com>
2026-05-14 14:48:27 +0900

    ㄴㅏ루 인트로 페이지 완성~

---
+++ homepage/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Intro from './pages/Intro'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
