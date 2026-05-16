a7227901a9c55e6ec5c735f970256a7ca7c09115
김이레 <165885725+elroi773@users.noreply.github.com>
2026-05-16 22:49:22 +0900

    feat: overview 페이지와 전역 인터랙션 컴포넌트 연결

--- homepage/src/App.jsx
+++ homepage/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import GlobalCursor from './components/GlobalCursor'
+import SmoothScroll from './components/SmoothScroll'
 import Intro from './pages/Intro'
+import Overview from './pages/Overview'
 
 function App() {
   return (
     <BrowserRouter>
+      <SmoothScroll />
+      <GlobalCursor />
       <Routes>
         <Route path="/" element={<Intro />} />
+        <Route path="/overview" element={<Overview />} />
       </Routes>
     </BrowserRouter>
   )
