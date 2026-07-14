76efb265f2c0b1df7c248355558b1323de679bed
김이레 <165885725+elroi773@users.noreply.github.com>
2026-07-15 08:07:16 +0900

    refactor:클린코드로 코드 정리

---
+++ homepage/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import FeedBack from './FeedBack'
+import Intro from './Intro'
+import Overview from './Overview'
+import Research from './Research'
+import Result from './Result'
+import Ui1 from './Ui1'
+import Ui2 from './Ui2'
+import Ui3 from './Ui3'
+
+export default function Home() {
+  return (
+    <>
+      <Intro />
+      <Overview />
+      <Research />
+      <Result />
+      <Ui1 />
+      <Ui2 />
+      <Ui3 />
+      <FeedBack />
+    </>
+  )
+}
